7916f15b7cfe5e716a13bf70beb352fc7ba185b3
Rafael Cruz <rcas2009@outlook.com>
2024-03-21 16:48:26 -0300

    Fetching request parameters

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 from flask import Flask
-from flask_restx import Api, Resource
+from flask_restx import Api, Resource, reqparse
 
 app = Flask(__name__)
 api = Api(app)
 
+parser = reqparse.RequestParser()
+parser.add_argument('name', help='Specify your name')
+
 @api.route('/home')
 class HomePage(Resource):
+    @api.doc(parser=parser)
     def get(self):
-        return "This is the home page"
+        args = parser.parse_args()
+        name = args['name']
+        return f"This is the home page, this is your name: {name}"
 
 
 if __name__ == "__main__":
